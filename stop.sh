a1668777acac37cd6de3bb5c6d6eee538a9f83d7
AngeLoreno04 <dicandiangelorenzo@gmail.com>
2025-09-09 18:50:00 +0200

    refactor stop.sh

--- stop.sh
+++ stop.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
+# Ferma la lettura avviata con leggi.sh
 
 if [ -f /tmp/pico_tts_pid ]; then
     PID=$(cat /tmp/pico_tts_pid)
     if kill -0 $PID 2>/dev/null; then
         kill $PID
-        rm /tmp/pico_tts_pid
         echo "⏹️ Lettura interrotta."
     else
         echo "Nessuna lettura attiva."
-        rm /tmp/pico_tts_pid
     fi
+    rm /tmp/pico_tts_pid
 else
     echo "Nessuna lettura in corso."
 fi
-
